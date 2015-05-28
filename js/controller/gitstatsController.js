f5dcce11eeadb226c56cfc53891d11159be0229f
TOYIN DIPO <toyindipol@gmail.com>
2015-05-28 14:22:18 +0100

    re upload

--- js/controller/gitstatsController.js
+++ js/controller/gitstatsController.js
@@ -4,34 +4,36 @@ app.controller("GitCtrl", function($scope, $http){
     $scope.orgNotFound = false;
     $scope.loaded = false;
 
-    $http.get("https://api.github.com/orgs/" + $scope.orgname + "/members")
+    $scope.authKey = "?client_id=92e95931025ef214002d&client_secret=a4c5fb9e89d0f073f65394933f4871a541c00818";
+    $scope.orgUrl = "https://api.github.com/orgs/";
+    $scope.usersUrl = "https://api.github.com/users/";
+    $scope.reposUrl = "https://api.github.com/repos/";
+    
+    $http.get($scope.orgUrl + $scope.orgname + "/members" + $scope.authKey)
       .success(function(data){
         $scope.members = data;
         $scope.loaded = true;
+        $scope.members.org = $scope.orgname;
         var i = 0;
+
         for (x in $scope.members){
-          $http.get("https://api.github.com/users/" + $scope.members[x].login + "/repos")
+          $http.get($scope.usersUrl + $scope.members[x].login + $scope.authKey)
             .success(function(data){
-              $scope.repos = data;
-              $scope.members[i].repoNum = $scope.repos.length;
-              ++i;
-              $scope.members[i].ghname = $scope.repos.name
-              for (y in $scope.repos){
-                // var total = 0;
-                $http.get("https://api.github.com/repos/" + $scope.members[x].login +"/" + $scope.repos[y].name + "/commits") 
-                  .success(function(data){
-                    // total += data.length;
-                    console.log(data.length);
-                  })
-                  .error(function( ){
-                    // total += 0;
-                  })
+              $scope.users = data;
+              if ($scope.members[i]){
+                $scope.members[i].userName = $scope.users.name;
+                $scope.members[i].repoNum = $scope.users.public_repos;
+                $scope.members[i].followers = $scope.users.followers;
+                $scope.members[i].following = $scope.users.following;
+                ++i
               }
-              // console.log(total);
+            })
+            .error(function(){
+
             })
         }
       })
-      .error(function( ){
+      .error(function(){
         $scope.orgNotFound = true;
       })
   }
