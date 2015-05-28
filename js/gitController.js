98e6db90b4ad5e22949870c95cffe08e0b85c53b
Oluwayemisi <oluwayemisioduye@gmail.com>
2015-05-28 11:28:01 +0100

    interface and list of members of organisation ordered with number of repos

---
+++ js/gitController.js
@@ -0,0 +1,35 @@
+var app = angular.module("gitHub", []);
+app.controller("GitCtrl", function($scope, $http){
+  $scope.orgNotFound = false;
+  $scope.loaded = false;
+
+  $http.get("https://api.github.com/orgs/andela/members")
+    .success(function(data){
+      $scope.members = data;
+      $scope.loaded = true;
+      var i = 0;
+      for (x in $scope.members){
+        $http.get("https://api.github.com/users/" + $scope.members[x].login + "/repos")
+          .success(function(data){
+            $scope.repos = data;
+            $scope.members[i].repoNum = $scope.repos.length;
+            ++i;
+            $scope.members[i].ghname = $scope.repos.name
+            for (y in $scope.repos){
+              var total = 0;
+              $http.get("https://api.github.com/repos/" + $scope.members[x].login +"/" + $scope.repos[y].name + "/commits") 
+                .success(function(data){
+                  total += data.length;
+                })
+                .error(function( ){
+                  total += 0;
+                })
+            }
+            console.log(total);
+          })
+      }
+    })
+    .error(function( ){
+      $scope.orgNotFound = true;
+    })
+})
